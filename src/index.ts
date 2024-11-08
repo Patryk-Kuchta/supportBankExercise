7133b56655ee1bb5cdd9a9af354f99b8c2775a45
Patryk-Kuchta <patryk.kuchta@softwire.com>
2024-11-08 13:59:33 +0000

    expand the classes to include all the required information and parse the transactions

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ class Account {
   }
 
   public toString(): string {
-    return `${this.name} has a balance of £${this.balance}.`
+    return `${this.name} (£${this.balance}).`
   }
 
   private static nameToAccount: Map<string, Account> = new Map()
@@ -32,12 +32,27 @@ class Transaction {
   public origin: Account
   public destination: Account
   public amount: number
+  public date: Date
 
   private constructor(
     originName: string,
     destinationName: string,
-    amount: number
+    amount: number,
+    dateString: string
   ) {
+    this.origin = Account.getOrCreate(originName)
+    this.destination = Account.getOrCreate(destinationName)
     this.amount = amount
+    this.date = new Date(dateString)
+  }
+
+  public toString(): string {
+    return `${this.origin.toString()} sent ${this.amount} to 
+            ${this.destination.toString()}`
+  }
+
+  public parseTransaction(csvEntry: string) {
+    let parts = csvEntry.split(',')
+    return new Transaction(parts[1], parts[2], Number(parts[3]), parts[0])
   }
 }
