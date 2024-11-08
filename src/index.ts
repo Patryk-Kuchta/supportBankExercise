9d702d6687d942d28f1d973feb848b2c64b3085f
Patryk-Kuchta <patryk.kuchta@softwire.com>
2024-11-08 15:43:38 +0000

    implements the requested functions, without the userInput

--- src/index.ts
+++ src/index.ts
@@ -132,10 +132,26 @@ function loadTheTransactionFile(filename: string) {
 
 loadTheTransactionFile('./data/Transactions2014.csv')
 
-console.log(
-  Account.getAccountNames()
-    .map((account) => {
-      return Account.getAccountWithName(account).getAccountStatement()
-    })
-    .join('\n\n')
-)
+function listAll() {
+  console.log(
+    Account.getAccountNames()
+      .map((account) => {
+        return Account.getAccountWithName(account).toString()
+      })
+      .join('\n')
+  )
+}
+
+function listAccount(accountName: string) {
+  let accountFound = Account.getAccountWithName(accountName)
+
+  if (accountFound) {
+    console.log(accountFound.getAccountStatement())
+  } else {
+    throw new Error('Account with the provided name does not exist.')
+  }
+}
+
+listAll()
+console.log('-')
+listAccount('Chris W')
