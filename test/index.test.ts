8e8f9e590c21c15ee2a199899a1fea8f096fbfb7
Patryk-Kuchta <patryk.kuchta@softwire.com>
2024-11-11 13:58:56 +0000

    test existing receivers

--- test/index.test.ts
+++ test/index.test.ts
@@ -89,57 +89,55 @@ describe('CSV Transaction Parsing', () => {
 
   })
 
-  it('valid transaction, new users', async () => {
-    const inputDetailed : DetailedInput = {
-      date: {
-        input: "01/02/1998",
-        output: "01 Feb 1998"
-      },
-      sender: {
-        input: "Sender A"
-      },
-      receiver: {
-        input: "Receiver B"
-      },
-      narrative : {
-        input: "With the intent of testing the system"
-      },
-      amount: {
-        input: 7.81
-      }
-    }
-    const inputLine = detailedInputIntoCSVLine(inputDetailed);
+  describe('valid transactions', () => {
 
-    const output = Transaction.parseTransaction(inputLine);
-    const transactionRepresentation = output.transaction.toString()
+    describe('with existing users', () => {
 
-    // starts with the correct date
-    expect(transactionRepresentation).toMatch(
-      new RegExp(`^\\[${inputDetailed.date.output}\\] `))
+      const inputDetailed: DetailedInput = {
+        date: {
+          input: "01/02/1998",
+          output: "01 Feb 1998"
+        },
+        sender: {
+          input: 'Existing Sender'
+        },
+        receiver: {
+          input: 'Existing Receiver'
+        },
+        narrative: {
+          input: "With the intent of testing the system"
+        },
+        amount: {
+          input: 7.81
+        }
+      };
 
-    // lists the correct sender
-    expect(transactionRepresentation).toMatch(
-      new RegExp(`\\] ${inputDetailed.sender.input} \\(`))
+      const sender = Account.getAccountWithName(inputDetailed.sender.input, true);
+      const receiver = Account.getAccountWithName(inputDetailed.receiver.input, true);
 
-    // lists the correct amount
-    expect(transactionRepresentation).toMatch(
-      new RegExp(`\\) sent £${inputDetailed.amount.input.toFixed(2)} to`))
+      const inputLine = detailedInputIntoCSVLine(inputDetailed);
+      const output = Transaction.parseTransaction(inputLine);
+      const transactionRepresentation = output.transaction.toString();
 
-    // lists the receiver sender
-    expect(output.transaction.toString()).toMatch(
-      new RegExp(` to ${inputDetailed.receiver.input} \\(`))
+      it('should list the correct sender', () => {
+        expect(transactionRepresentation).toMatch(
+          new RegExp(`\\] ${inputDetailed.sender.input} \\(`)
+        );
+      });
 
-    // lists the correct narrative
-    expect(output.transaction.toString()).toMatch(
-      new RegExp(` for "${inputDetailed.narrative.input}"\$`))
+      it('should list the receiver sender correctly', () => {
+        expect(transactionRepresentation).toMatch(
+          new RegExp(` to ${inputDetailed.receiver.input} \\(`)
+        );
+      });
 
-    // stores the correct amount
-    expect(output.transaction.getAmountDue()).toBeCloseTo(inputDetailed.amount.input)
+      it('should link to the correct sender account', () => {
+        expect(output.origin).toBe(sender);
+      });
 
-    // links to the correct sender
-    expect(output.origin).toBe(Account.getAccountWithName(inputDetailed.sender.input))
-
-    // links to the correct receiver
-    expect(output.destination).toBe(Account.getAccountWithName(inputDetailed.receiver.input))
+      it('should link to the correct receiver account', () => {
+        expect(output.destination).toBe(receiver);
+      });
+    });
   })
 });
