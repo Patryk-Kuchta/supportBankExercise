c59b4a106bbecd62f489a1ca9f83ca3f38658da3
Patryk-Kuchta <patryk.kuchta@softwire.com>
2024-11-11 09:51:56 +0000

    set up the logger and naively load the dodgy file

--- src/Initialize.ts
+++ src/Initialize.ts
@@ -1,9 +1,22 @@
 import { readFileSync } from "fs";
 import Transaction from "./Transaction";
+import log4js from "log4js";
 
 function initialize() {
+  log4js.configure({
+    appenders: {
+      file: { type: "fileSync", filename: "logs/debug.log" },
+    },
+    categories: {
+      default: { appenders: ["file"], level: "debug" },
+    },
+  });
+
   function loadTheTransactionFile(filename: string) {
+    const logger = log4js.getLogger("logs/debug.log");
+
     const data = readFileSync(filename, "utf8");
+    logger.info(`${filename} opened successfully.`);
 
     for (let line of data.split("\n").slice(1)) {
       if (line.length > 0) {
@@ -13,6 +26,7 @@ function initialize() {
   }
 
   loadTheTransactionFile("./data/Transactions2014.csv");
+  loadTheTransactionFile("./data/DodgyTransactions2015.csv");
 }
 
 export default initialize;
