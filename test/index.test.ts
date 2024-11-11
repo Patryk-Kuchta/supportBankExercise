2720c6788e86afe0daf38d99dfebd7aaa0254b05
Patryk-Kuchta <patryk.kuchta@softwire.com>
2024-11-11 14:50:23 +0000

    adds statement tests (100% coverage :) )

--- test/index.test.ts
+++ test/index.test.ts
@@ -20,12 +20,13 @@ type DetailedInput = {
   amount: InputAndOptionallyOutput<number>
 }
 
+function detailedInputIntoCSVLine(detailedInput: DetailedInput) {
+  return `${detailedInput.date.input},${detailedInput.sender.input},` +
+    `${detailedInput.receiver.input},${detailedInput.narrative.input},` +
+    `${detailedInput.amount.input}`;
+}
+
 describe('CSV Transaction Parsing', () => {
-  function detailedInputIntoCSVLine(detailedInput: DetailedInput) {
-    return `${detailedInput.date.input},${detailedInput.sender.input},` +
-           `${detailedInput.receiver.input},${detailedInput.narrative.input},` +
-           `${detailedInput.amount.input}`;
-  }
 
   describe('valid transactions', () => {
 
@@ -214,7 +215,122 @@ describe('CSV Transaction Parsing', () => {
       });
     });
   });
-
-
-
 });
+
+describe('Account', () => {
+  it('should prevent creating an account with an existing name', () => {
+    new Account("I am duplicated")
+    expect(() => new Account("I am duplicated")).toThrow(Error)
+  })
+
+  it('should print all existing account names', () => {
+    Account['nameToAccount'] = new Map<string, Account>() // reset
+
+    const names = ['A B', 'B C', 'C D'];
+
+    for (const name of names) {
+      new Account(name);
+    }
+
+    const accountNameList = Account.getAccountNames()
+
+    expect(accountNameList).toEqual(names);
+  })
+})
+
+describe('Bank statement', () => {
+  const simulatedTransactions : DetailedInput[] = [
+    {
+      date: {
+        input: "01/02/1998",
+        output: "01 Feb 1998"
+      },
+      sender: {
+        input: "A"
+      },
+      receiver: {
+        input: "B"
+      },
+      narrative: {
+        input: "First Test Transaction"
+      },
+      amount: {
+        input: 7.81
+      }
+    },
+    {
+      date: {
+        input: "01/02/1998",
+        output: "01 Feb 1998"
+      },
+      sender: {
+        input: "B"
+      },
+      receiver: {
+        input: "A"
+      },
+      narrative: {
+        input: "Second Test Transaction"
+      },
+      amount: {
+        input: 2.31
+      }
+    }
+  ]
+
+  let balance = 0;
+
+  for (const transaction of simulatedTransactions) {
+    const input = detailedInputIntoCSVLine(transaction)
+    Transaction.parseTransaction(input)
+
+    if (transaction.sender.input == "A") {
+      balance -= transaction.amount.input;
+    } else {
+      balance += transaction.amount.input;
+    }
+  }
+
+  const accountStatement = Account.getAccountWithName('A').getAccountStatement()
+
+  it('should state the correct name and balance (on Side A)', () => {
+    expect(accountStatement).toMatch(
+      new RegExp(`^A \\(£${balance.toFixed(2)}\\)\n`)
+    );
+  });
+
+  for (const transaction of simulatedTransactions) {
+    describe(transaction.narrative.input, () => {
+
+      it('should start with the correct date (on Side A)', () => {
+        expect(accountStatement).toMatch(
+          new RegExp(`\n\\[${transaction.date.output}\\] `)
+        );
+      });
+
+      it('should list the correct sender (on Side A)', () => {
+        expect(accountStatement).toMatch(
+          new RegExp(`\\] ${transaction.sender.input} \\(`)
+        );
+      });
+
+      it('should list the correct amount (on Side A)', () => {
+        expect(accountStatement).toMatch(
+          new RegExp(`\\) sent £${transaction.amount.input.toFixed(2)} to`)
+        );
+      });
+
+      it('should list the receiver sender correctly (on Side A)', () => {
+        expect(accountStatement).toMatch(
+          new RegExp(` to ${transaction.receiver.input} \\(`)
+        );
+      });
+
+      it('should list the correct narrative (on Side A)', () => {
+        expect(accountStatement).toMatch(
+          new RegExp(` for "${transaction.narrative.input}"`)
+        );
+      });
+    })
+  }
+})
