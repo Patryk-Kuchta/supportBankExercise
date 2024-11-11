7f9e29721141abfd76b8adb72f7057162db95bbf
Patryk-Kuchta <patryk.kuchta@softwire.com>
2024-11-11 09:13:18 +0000

    replace ' with " for consistency and adding semicolon

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,45 @@
-import { question } from 'readline-sync'
-import { listAccount, listAll } from './UserActions'
-import initialize from './Initialize'
+import { question } from "readline-sync";
+import { listAccount, listAll } from "./UserActions";
+import initialize from "./Initialize";
 
-initialize()
+initialize();
 
-let userAnswer = 'HELP'
+let userAnswer = "HELP";
 
-while (userAnswer !== 'QUIT') {
+while (userAnswer !== "QUIT") {
   switch (userAnswer) {
-    case 'HELP': {
-      console.log('Available commands:')
+    case "HELP": {
+      console.log("Available commands:");
       console.log(
         "  LIST ALL       - Lists each person's name and total amount owed or owed to them."
-      )
+      );
       console.log(
-        '  LIST [Account] - Lists every transaction for the specified account by name.'
-      )
-      console.log('  HELP           - Shows this available commands list.')
-      console.log('  QUIT           - Exits the program.')
-      break
+        "  LIST [Account] - Lists every transaction for the specified account by name."
+      );
+      console.log("  HELP           - Shows this available commands list.");
+      console.log("  QUIT           - Exits the program.");
+      break;
     }
-    case 'LIST ALL': {
-      listAll()
-      break
+    case "LIST ALL": {
+      listAll();
+      break;
     }
     case userAnswer.match(/^LIST\s+(.+)/)?.input: {
-      const accountName = userAnswer.replace(/^LIST\s+/i, '').trim()
-      console.log(accountName)
+      const accountName = userAnswer.replace(/^LIST\s+/i, "").trim();
+      console.log(accountName);
 
-      listAccount(accountName)
-      break
+      listAccount(accountName);
+      break;
     }
     default: {
       console.log(
-        'Unknown command. Type HELP for a list of available commands.'
-      )
-      break
+        "Unknown command. Type HELP for a list of available commands."
+      );
+      break;
     }
   }
 
-  userAnswer = question('Enter command: ')
+  userAnswer = question("Enter command: ");
 }
 
-console.log('Goodbye!')
+console.log("Goodbye!");
