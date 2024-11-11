a104c004a32226aae48c41adfec387815592327e
Patryk-Kuchta <patryk.kuchta@softwire.com>
2024-11-11 09:23:25 +0000

    finish the eslint fixes

--- src/Account.ts
+++ src/Account.ts
@@ -1,67 +1,67 @@
-import Transaction from './Transaction'
+import Transaction from "./Transaction";
 
 class Account {
-  private readonly name: string
-  private balance: number
+  private readonly name: string;
+  private balance: number;
 
-  private readonly incomingTransactions: Transaction[]
-  private readonly outgoingTransactions: Transaction[]
+  private readonly incomingTransactions: Transaction[];
+  private readonly outgoingTransactions: Transaction[];
 
   public constructor(name: string, balance = 0) {
     if (Account.nameToAccount.has(name)) {
-      throw new Error('Account with this name already exists')
+      throw new Error("Account with this name already exists");
     }
 
-    this.name = name
-    this.balance = balance
+    this.name = name;
+    this.balance = balance;
 
-    this.incomingTransactions = []
-    this.outgoingTransactions = []
+    this.incomingTransactions = [];
+    this.outgoingTransactions = [];
 
-    Account.nameToAccount.set(name, this)
+    Account.nameToAccount.set(name, this);
   }
 
   public toString(): string {
-    return `${this.name} (£${this.balance.toFixed(2)})`
+    return `${this.name} (£${this.balance.toFixed(2)})`;
   }
 
   public getAccountStatement(): string {
-    let result = this.toString() + '\n'
-    result += '*'.repeat(5) + ' Incoming Transactions ' + '*'.repeat(5) + '\n'
+    let result = this.toString() + "\n";
+    result += "*".repeat(5) + " Incoming Transactions " + "*".repeat(5) + "\n";
     for (let transaction of this.incomingTransactions) {
-      result += transaction.toString() + '\n'
+      result += transaction.toString() + "\n";
     }
-    result += '*'.repeat(5) + ' Outgoing Transactions ' + '*'.repeat(5) + '\n'
+    result += "*".repeat(5) + " Outgoing Transactions " + "*".repeat(5) + "\n";
     for (let transaction of this.outgoingTransactions) {
-      result += transaction.toString() + '\n'
+      result += transaction.toString() + "\n";
     }
-    result += '*'.repeat(30)
-    return result
+    result += "*".repeat(30);
+    return result;
   }
 
-  private static nameToAccount: Map<string, Account> = new Map()
+  private static nameToAccount: Map<string, Account> = new Map();
 
   public static getAccountWithName(name: string, createIfNotExistent = false) {
     if (Account.nameToAccount.has(name)) {
-      return Account.nameToAccount.get(name)
+      return Account.nameToAccount.get(name);
     } else if (createIfNotExistent) {
-      return new Account(name)
+      return new Account(name);
     }
   }
 
   public addIncomingTransaction(transaction: Transaction) {
-    this.balance += transaction.getAmountDue()
-    this.incomingTransactions.push(transaction)
+    this.balance += transaction.getAmountDue();
+    this.incomingTransactions.push(transaction);
   }
 
   public addOutgoingTransaction(transaction: Transaction) {
-    this.balance -= transaction.getAmountDue()
-    this.outgoingTransactions.push(transaction)
+    this.balance -= transaction.getAmountDue();
+    this.outgoingTransactions.push(transaction);
   }
 
   public static getAccountNames() {
-    return Array.from(this.nameToAccount.keys())
+    return Array.from(this.nameToAccount.keys());
   }
 }
 
-export default Account
+export default Account;
--- src/Transaction.ts
+++ src/Transaction.ts
@@ -1,12 +1,12 @@
-import moment, { Moment } from 'moment'
-import Account from './Account'
+import moment, { Moment } from "moment";
+import Account from "./Account";
 
 class Transaction {
-  private readonly origin: Account
-  private readonly destination: Account
-  private readonly amount: number
-  private readonly date: Moment
-  private readonly narrative: string
+  private readonly origin: Account;
+  private readonly destination: Account;
+  private readonly amount: number;
+  private readonly date: Moment;
+  private readonly narrative: string;
 
   private constructor(
     originName: Account,
@@ -15,29 +15,29 @@ class Transaction {
     dateString: string,
     narrative: string
   ) {
-    this.origin = originName
-    this.destination = destinationName
-    this.amount = amount
-    this.date = moment(dateString, 'DD/MM/YYYY')
-    this.narrative = narrative
+    this.origin = originName;
+    this.destination = destinationName;
+    this.amount = amount;
+    this.date = moment(dateString, "DD/MM/YYYY");
+    this.narrative = narrative;
   }
 
   public toString(): string {
     return (
-      `[${this.date.format('DD MMM YYYY')}] ${this.origin.toString()} sent £` +
+      `[${this.date.format("DD MMM YYYY")}] ${this.origin.toString()} sent £` +
       `${this.amount.toFixed(2)} to ${this.destination.toString()} for ` +
       `"${this.narrative}"`
-    )
+    );
   }
 
   public getAmountDue(): number {
-    return this.amount
+    return this.amount;
   }
 
   public static parseTransaction(csvEntry: string) {
-    const parts = csvEntry.split(',')
-    const origin = Account.getAccountWithName(parts[1], true)
-    const destination = Account.getAccountWithName(parts[2], true)
+    const parts = csvEntry.split(",");
+    const origin = Account.getAccountWithName(parts[1], true);
+    const destination = Account.getAccountWithName(parts[2], true);
 
     return {
       transaction: new Transaction(
@@ -49,8 +49,8 @@ class Transaction {
       ),
       origin,
       destination,
-    }
+    };
   }
 }
 
-export default Transaction
+export default Transaction;
